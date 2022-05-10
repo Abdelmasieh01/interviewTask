19f4637baf4469be5ab5a6d6ebcce851f918b403
Abdelmasieh Baselious <gokuabdo31@gmail.com>
2022-05-10 23:23:19 +0200

    Adding more tests.

--- task_state_api/tasks_api/tests.py
+++ task_state_api/tasks_api/tests.py
@@ -1,5 +1,5 @@
 from .models import Task
-from rest_framework.test import APIClient, APITestCase
+from rest_framework.test import APITestCase
 from rest_framework import status
 
 class TaskTestCase(APITestCase):
@@ -13,13 +13,27 @@ class TaskTestCase(APITestCase):
 
     def test_update_task(self):
         Task.objects.create(title='Testing update.', state='ac')
-        
         data = {'title': 'Testing update.', 'state': 'dn'}
-        response = self.client.put('/api/tasks/1/', data, format='json')
+        response = self.client.put('/api/tasks/1/', data)
         self.assertEqual(response.status_code, status.HTTP_202_ACCEPTED)
-        self.assertEqual(Task.objects.get().state, 'dn.')
+        self.assertEqual(Task.objects.get().state, 'dn')
+    
+    def test_update_not_found(self):
+        Task.objects.create(title='Testing Update, item not found.')
+        count = Task.objects.all().count()
+        count += 1
+        data = {'title': 'Testing update.', 'state': 'dn'}
+        response = self.client.put('/api/taks/' +  str(count) + '/', data)
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+    
+    def test_delete_task(self):
+        Task.objects.create(title='Testing delete.', state='ac')
+        response = self.client.delete('/api/tasks/1/')
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
     
     
+
+
 
 
 
